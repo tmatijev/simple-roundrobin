9319f8fce7a1150fd159db1933a43a8adaf89e86
tmatijev <matas0412@gmail.com>
2024-12-16 16:19:26 +0100

    Remove not used test file

--- test-usage.js
+++
@@ -1,6 +0,0 @@
-const generateRoundRobin = require('./dist/index.js');
-
-const teams = ['Real Madrid', 'Barcelona', 'Atletico', 'Valencia'];
-const schedule = generateRoundRobin({ clubs: teams, legs: 2 });
-
-console.log(JSON.stringify(schedule, null, 2));
